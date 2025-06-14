a1a4cc1c1643c305ac7c524e8b329d46979e4a94
Renato Aguilar <renato.aguilar@epn.edu.ec>
2025-06-14 00:42:15 -0500

    primer commit de proyecto java

---
+++ src/pruebaGit/PrincipalGola.java
@@ -0,0 +1,10 @@
+package pruebaGit;
+
+public class PrincipalGola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Te odio windows");
+	}
+
+}
