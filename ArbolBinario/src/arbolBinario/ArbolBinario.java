4bfcaf3af665f92348c447b7a34c177f48150e6e
Renato Aguilar <renato.aguilar@epn.edu.ec>
2025-06-25 12:48:33 -0500

    Codigo final arbol binario

--- ArbolBinario/src/arbolBinario/ArbolBinario.java
+++ ArbolBinario/src/arbolBinario/ArbolBinario.java
@@ -2,6 +2,7 @@ package arbolBinario;
 
 public class ArbolBinario {
 	private NodoArbol raiz;
+	private int nivel=0;
 	public boolean esVacio() {
 		return raiz==null;
 	}
@@ -18,7 +19,7 @@ public class ArbolBinario {
 			this.raiz.insertarNodo(valor);//Se empieza a decidir donde colocar el nodo desde la raiz, va bajando segun las llamadas recursivas
 		}
 	}
-	//Recorrido preorden
+	//Recorrido preorden: R-I-D
 	public void EjecutarPreOrder() {
 		PreOrder(raiz);
 	}
@@ -31,5 +32,65 @@ public class ArbolBinario {
 			PreOrder(nodo.getDerecha()); //Recorre el subarbol derecho (D)
 		}
 	}
-	
+	//Recorrido preorden: I-R-D
+	public void EjecutarInOrder() {
+		InOrder(raiz);
+	}
+	public void InOrder(NodoArbol nodo) {
+		if(nodo == null) { //Caso base
+			return;
+		}else {
+			InOrder(nodo.getIzquierda()); //Recorre el subarbol izquierdo (I)
+			System.out.print(nodo.getDato() + "-"); //Visita la raiz (R)
+			InOrder(nodo.getDerecha()); //Recorre el subarbol derecho (D)
+		}
+	}
+	//Recorrido preorden: I-D-R
+	public void EjecutarPostOrder() {
+		PostOrder(raiz);
+	}
+	public void PostOrder(NodoArbol nodo) {
+		if(nodo == null) { //Caso base
+			return;
+		}else {
+			PostOrder(nodo.getIzquierda()); //Recorre el subarbol izquierdo (I)
+			PostOrder(nodo.getDerecha()); //Recorre el subarbol derecho (D)
+			System.out.print(nodo.getDato() + "-"); //Visita la raiz (R)
+		}
+	}
+	//Buscar nodo del arbol
+	public void ejecutarBuscarNodo(int valor) {
+		buscarNodo(raiz, valor);
+	}
+	public void buscarNodo(NodoArbol nodo, int dato) {
+		if(esVacio()) {
+			System.out.println("Arbol vacio, nada por buscar");
+			
+			return;
+		}
+		if(nodo == null) {
+			System.out.println("Elemento "+dato+" no se encontro");
+			return;
+		}
+		if(nodo.dato == dato) {
+			System.out.println("Elemento "+dato+" encontrado en el nivel " + nivel);
+			nivel = 0;
+			return;
+		}
+		if(dato<nodo.dato) { 
+			System.out.println("Buscando en el subarbol izquierdo");
+			contarNiveles();
+			buscarNodo(nodo.getIzquierda(), dato);
+			
+			
+		}else {
+			System.out.println("Buscando en el subarbol derecho");
+			contarNiveles();
+			buscarNodo(nodo.getDerecha(), dato);
+		}	
+	}
+	//Contar niveles{
+	public void contarNiveles() {
+		nivel++;
+	}
 }
--- ArbolBinario/src/arbolBinario/PrincipalArbol.java
+++ ArbolBinario/src/arbolBinario/PrincipalArbol.java
@@ -1,20 +1,60 @@
 package arbolBinario;
 
+import java.util.Scanner;
+
 public class PrincipalArbol {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		ArbolBinario arbol1 = new ArbolBinario();
-		arbol1.insertar(10);
-		arbol1.insertar(5);
-		arbol1.insertar(15);
-		arbol1.insertar(6);
-		arbol1.insertar(17);
-		arbol1.insertar(20);
+		int opcion;
+		do {
+			System.out.println();
+			System.out.println("1. Insertar Nodo");
+			System.out.println("2. Buscar nodo");
+			System.out.println("3. Imprimir PreOrder");
+			System.out.println("4. Imprimir InOrder");
+			System.out.println("5. Imprimir PostOrder");
+			System.out.println("6. Salir");
+			System.out.println("Ingrese la opcion deseada: ");
+			opcion= sc.nextInt();
+			switch(opcion) {
+			case 1:
+				System.out.println("--INSERTAR NODO--");
+				System.out.println("Ingrese un valor: ");
+				int nodo = sc.nextInt();
+				arbol1.insertar(nodo);
+				break;
+			case 2:
+				System.out.println("--BUSCAR NODO--");
+				System.out.println("Ingrese un valor a buscar: ");
+				int aBuscar = sc.nextInt();
+				arbol1.ejecutarBuscarNodo(aBuscar);
+				break;
+			case 3:
+				System.out.println("--PRE ORDER--");
+				arbol1.EjecutarPreOrder();
+				break;
+			case 4:
+				System.out.println("--IN ORDER--");
+				arbol1.EjecutarInOrder();
+				break;
+			case 5:
+				System.out.println("--POST ORDER--");
+				arbol1.EjecutarPostOrder();
+				break;
+			case 6:
+				System.out.println("Gracias por usar el programa :)");
+				break;
+			default:
+				System.out.println("Ingrese una opcion valida");
+				break;
+			}
+		}while(opcion != 6);
 		
 		
-		//arbol1.PreOrder(arbol1.getRaiz());
-		arbol1.EjecutarPreOrder();
+		
 	}
 
 }
